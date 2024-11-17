749450ba0220e0e4461256c75a880c51b2efc0df
ivandalechenko <ivandalechenko@gmail.com>
2024-11-17 20:41:32 +0200

    added chart

--- numbers.js
+++ numbers.js
@@ -15,20 +15,20 @@ export default (gsap) => {
         }
     })
 
-    gsap.fromTo('.numbers_chart', {
-        x: -200,
-        opacity: 0,
-    }, {
-        x: 0,
-        opacity: 1,
-        scrollTrigger: {
-            trigger: '.numbers',
-            scrub: 1,
-            // markers: true,
-            start: `${persentPerEl * (start + 3)}px 80% `,
-            end: `${persentPerEl * (start + 3)}px 80% `,
-        }
-    })
+    // gsap.fromTo('.numbers_chart', {
+    //     x: -200,
+    //     opacity: 0,
+    // }, {
+    //     x: 0,
+    //     opacity: 1,
+    //     scrollTrigger: {
+    //         trigger: '.numbers',
+    //         scrub: 1,
+    //         // markers: true,
+    //         start: `${persentPerEl * (start + 3)}px 80% `,
+    //         end: `${persentPerEl * (start + 3)}px 80% `,
+    //     }
+    // })
     gsap.fromTo('.numbers_decor', {
         x: 200,
         opacity: 0,
